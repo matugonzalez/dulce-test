91346c48370e98adb0a6b027d4121cfa97e12975
Mateo González <minidrw@gmail.com>
2023-10-29 20:34:43 -0300

    feat: changes to home page component

--- src/components/HeaderBar/index.jsx
+++ src/components/HeaderBar/index.jsx
@@ -9,21 +9,23 @@ const HeaderBar = () => {
     const [state] = useAdminSystem()
     const navigate = useNavigate()
     return (
-        <header>
-            <Logo className='HeaderBar__Logo' />
-            <nav>
+        <header className='HeaderBar'>
+            <Logo className='HeaderBar__BrandLogo' />
+            <div className='HeaderBar__BTSet'>
                 <Button onClickFunction={() => navigate('/home')}>HOME</Button>
                 <Button onClickFunction={() => navigate('/menu')}>MENU</Button>
                 <Button onClickFunction={() => navigate('/orders')}>ORDERS</Button>
-                <Button onClickFunction={() => navigate('/about-us')}>ABOUT US</Button>
                 <Button onClickFunction={() => navigate('/contact')}>CONTACT</Button>
-            </nav>
+            </div>
             <div>
                 {state.userSession.logged 
                     ? <div>Logged as {state.userSession.loginInfo.username}</div>
-                    : <Button onClickFunction={() => navigate('/login')}>Log In</Button>
+                    : 
+                    <div className='HeaderBar__BTSet'>
+                        <Button onClickFunction={() => navigate('/login')}>Log In</Button>
+                        <Button onClickFunction={() => navigate('/register')}>Register</Button>
+                    </div>
                 }
-                <UserLogo className='UserLogo' />
             </div>
         </header>
   )
