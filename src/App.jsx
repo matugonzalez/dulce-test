6f3520129b840d22f0a615e4e2004ab1a641a561
NAxTIM17 <naxtim14@gmail.com>
2023-10-30 13:12:43 -0300

    feat:Create missing pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
 import './App.css'
-
 import BusinessRouter from './components/BusinessRouter';
 
 function App() {
--- src/pages/Admin/Dashboard/index.jsx
+++ src/pages/Admin/Dashboard/index.jsx
@@ -1,21 +1,19 @@
 import './Dashboard.css'
 import { useAdminSystem } from '../../../providers/AdminSystem'
-import { useNavigate } from 'react-router-dom'
-import Button from '../../../components/Button'
 import PreviousPageBT from '../../../components/PreviousPageBT'
+import NavsLinks from '../../../components/NavLinks'
 
 
 const Dashboard = () => {
     const [state] = useAdminSystem()
-    const navigate = useNavigate()
     
     return (
         <div className='Dashboard'>
             <h1>Welcome {state.userSession.loginInfo.username}</h1>
             <div className='Dashboard_container'>
-                <Button className='Home_linkButton' onClickFunction={() => navigate('/admin/stock')}><span>STOCK</span></Button>
-                <Button className='Home_linkButton' onClickFunction={() => navigate('/admin/orders')}><span>ORDERS</span></Button>
-                <Button className='Home_linkButton' onClickFunction={() => navigate('/admin/menu')}><span>MENU</span></Button>
+                <NavsLinks to = '/admin/stock' >STOCK</NavsLinks>
+                <NavsLinks to = '/admin/orders' >ORDERS</NavsLinks>
+                <NavsLinks to = '/admin/menu' >MENU</NavsLinks>
                 <PreviousPageBT />
             </div>
         </div>
