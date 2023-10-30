62e4c2965b4ed7b99ab2b4ce9b6ea04a3977ed71
Mateo González <minidrw@gmail.com>
2023-10-29 23:18:41 -0300

    feat: conditional render on navlinks

--- src/components/HeaderBar/index.jsx
+++ src/components/HeaderBar/index.jsx
@@ -1,38 +1,44 @@
 import './HeaderBar.css'
 import { useAdminSystem } from '../../providers/AdminSystem'
-import { useNavigate, NavLink } from 'react-router-dom'
 import Logo from '../Logo'
-import Button from '../Button'
-import UserLogo from '../Icons/UserLogo'
 import NavsLinks from '../NavLinks'
+import Button from '../Button'
 
 const HeaderBar = () => {
-    const [state] = useAdminSystem()
-    const navigate = useNavigate()
+    const [state, actions] = useAdminSystem()
+    console.log(state.userSession.logged)
     return (
-        <header>
-            <nav className='navbar'>
-                <Logo className='HeaderBar__Logo' />
-                    <div className="navsLink">
-                        <NavsLinks to = "/home">HOME</NavsLinks>
-                        <NavsLinks to = "/menu">MENU</NavsLinks>
-                        <NavsLinks to = "/orders">ORDERS</NavsLinks>
-                        <NavsLinks to = "/about-us">ABOUT US</NavsLinks>
-                        <NavsLinks to = "/contact">CONTACT</NavsLinks>
-                    </div>
+        <header className='HeaderBar'>
+            <Logo className='HeaderBar__Logo' />
+                <div className='navsLink'>
+                    <NavsLinks to = "/home">HOME</NavsLinks>
+                    <NavsLinks to = "/menu">MENU</NavsLinks>
+                    {state.userSession.logged && <NavsLinks to = "/orders">ORDERS</NavsLinks>}
+                    <NavsLinks to = "/about-us">ABOUT US</NavsLinks>
+                </div>
                 
-             </nav>
             <div>
                 {state.userSession.logged 
-                    ? <div>Logged as {state.userSession.loginInfo.username}</div>
+                    ? 
+                    <div className='navsLink'>
+                        Logged as {state.userSession.loginInfo.username}
+                        <Button onClickFunction={() => {
+                            actions.userSession.logOut()
+                        }}
+                        >
+                            LOG OUT
+                        </Button>
+                        {state.userSession.loginInfo.role === 'admin' && <NavsLinks to='/admin/dashboard'>DASHBOARD</NavsLinks>}
+                    </div>
                     : 
-                    <div className='HeaderBar__BTSet'>
-                        <Button onClickFunction={() => navigate('/login')}>Log In</Button>
-                        <Button onClickFunction={() => navigate('/register')}>Register</Button>
+                    <div className='navsLink'>
+                        <NavsLinks to = "/login">LOG IN</NavsLinks>
+                        <NavsLinks to = "/register">REGISTER</NavsLinks>
                     </div>
                 }
             </div>
         </header>
+
   )
 }
 
