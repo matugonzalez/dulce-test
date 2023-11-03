0356308fbb551428625827b01461fa31481b100c
Mateo González <minidrw@gmail.com>
2023-11-03 00:19:13 -0300

    feat: update route components

--- src/components/BusinessRouter/index.jsx
+++ src/components/BusinessRouter/index.jsx
@@ -1,16 +1,16 @@
-import { Routes, Route, Navigate, Outlet, useNavigate } from 'react-router-dom'
+import { Routes, Route, Navigate, useParams } from 'react-router-dom'
 import { useAdminSystem } from '../../providers/AdminSystem'
 import ProtectedRoute from './ProtectedRoute'
 
 // ADMIN PAGES
-import AdminOrders from '../../pages/Admin/AdminOrders'
-import AdminOrder from '../../pages/Admin/AdminOrder'
+import AdminOrders from '../../pages/Admin/AdminOrdersPage'
+import AdminOrder from '../../pages/Admin/AdminOrderPage'
 import Dashboard from '../../pages/Admin/Dashboard'
 import Stock from '../../pages/Admin/Stock'
+import AdminMenuPage from '../../pages/Admin/AdminMenuPage'
 
 // CLIENT PAGES
 import Home from '../../pages/Client/Home'
-import Menu from '../../pages/Client/Menu'
 import AboutUs from '../../pages/Client/AboutUs'
 import ClientOrders from '../../pages/Client/ClientOrders'
 import ClientOrder from '../../pages/Client/ClientOrder'
@@ -21,7 +21,7 @@ import LoginPage from '../../pages/LoginPage'
 
 const BusinessRouter = () => {
     const [state] = useAdminSystem()
-    const navigate = useNavigate()
+    const  {orderId}  = useParams()
     return (
         <Routes>
             {/* ADMIN RELATED ROUTES */}
@@ -33,7 +33,7 @@ const BusinessRouter = () => {
 
                         <Route path='orders'>
                             <Route index element={<AdminOrders />}/>
-                            <Route path=':id' element={<AdminOrder />} />
+                            <Route path=':orderId' element={<AdminOrder />} />
                         </Route>
 
                         <Route path='stock'>
@@ -41,10 +41,10 @@ const BusinessRouter = () => {
                             <Route path='new' element={<Stock/>} />
                         </Route>
 
-                        <Route path='menu' element={<Menu />}/>
+                        <Route path='menu' element={<AdminMenuPage />}/>
                         <Route path='users'>
                             <Route index element={<div>where list of users should go</div>}/>
-                            <Route path=':id' element={<div>where each user should go</div>}/>
+                            <Route path=':userId' element={<div>where each user should go</div>}/>
                         </Route>
                     </Route>
                 </Route>
@@ -60,13 +60,22 @@ const BusinessRouter = () => {
                             <Route path=':id' element={<ClientOrder />} />
                     </Route>
                 
-                    <Route path='menu' element={<Menu />}/>
+                    <Route path='menu' element={<div>where menu should go</div>}/>
 
                 </Route>
                 <Route path='*' element={<h1>404</h1>} />
             </Route>
   
-            <Route path='login' element={state.userSession.logged ? <Navigate to='/home'/> : <LoginPage />} />
+            <Route path='login' element={
+                state.userSession.logged
+                ? state.userSession.loginInfo.role === 'admin' 
+                    ?
+                    <Navigate to='/admin/menu'/>
+                    :
+                    <Navigate to='/home'/>
+                : <LoginPage />
+                }
+            />
             <Route path='register' element= {state.userSession.logged ?<Register to='/home'/> : <Register />} />
         </Routes>
         
